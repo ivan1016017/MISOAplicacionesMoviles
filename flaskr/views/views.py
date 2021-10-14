79beaf6b14d85b4a5d0a968fa2c35aa109ab0e2b
Ivan Penaloza <ivan1016017@gmail.com>
2021-10-14 14:01:50 -0500

    cleaning

--- flaskr/views/views.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource 
-from ..models  import db, User, UserSchema
-from flask import request
-from flask_jwt_extended import jwt_required, create_access_token
-
-user_schema = UserSchema()
