21837ac5801b57a967a5f4c5cee27fec3f35e80e
Wenceslao Paez <wcpaez@gmail.com>
2021-10-30 13:07:21 -0500

    Use retrofit

--- app/src/main/java/com/example/vynilos/adapters/AlbumAdapter.kt
+++ app/src/main/java/com/example/vynilos/adapters/AlbumAdapter.kt
@@ -9,42 +9,35 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vynilos.R
+import com.example.vynilos.databinding.ItemAlbumBinding
 import com.example.vynilos.models.Album
 import com.squareup.picasso.Picasso
 
-class AlbumAdapter : RecyclerView.Adapter<AlbumAdapter.AlbumHolder>() {
-    var albums: List<Album>  = ArrayList()
-    lateinit var context: Context
-
-    fun AlbumAdapter(albums : List<Album>, context: Context){
-        this.albums = albums
-        this.context = context
-    }
-
+class AlbumAdapter(val albums:List<Album>) : RecyclerView.Adapter<AlbumAdapter.AlbumHolder>() {
     override fun onBindViewHolder(holder: AlbumHolder, position: Int) {
-        val item = albums.get(position)
-        holder.bind(item, context)
+        val item = albums[position]
+        holder.bind(item)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return AlbumHolder(layoutInflater.inflate(R.layout.item_album, parent, false))
     }
+
     override fun getItemCount(): Int {
         return albums.size
     }
+
     class AlbumHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val name = view.findViewById(R.id.tvName) as TextView
-        val description = view.findViewById(R.id.tvDescription) as TextView
-        val genre = view.findViewById(R.id.tvGenre) as TextView
-        val recordLabel = view.findViewById(R.id.tvRecordLabel) as TextView
-        val cover = view.findViewById(R.id.ivCover) as ImageView
-        fun bind(album:Album, context: Context){
-            name.text = album.name
-            description.text = album.description
-            genre.text = album.genre
-            recordLabel.text = album.recordLabel
-            Picasso.get().load(album.cover).into(cover)
-            itemView.setOnClickListener(View.OnClickListener { Toast.makeText(context, "Clicked on $album.name", Toast.LENGTH_SHORT).show() })
+        private val binding = ItemAlbumBinding.bind(view)
+
+        fun bind(album:Album){
+            binding.tvName.text = album.name
+            binding.tvDescription.text = album.description
+            binding.tvGenre.text = album.genre
+            binding.tvRecordLabel.text = album.recordLabel
+            Picasso.get().load(album.cover).into(binding.ivCover)
+            //itemView.setOnClickListener(View.OnClickListener { Toast.makeText(context, "Clicked on $album.name", Toast.LENGTH_SHORT).show() })
         }
     }
 }
--- app/src/main/java/com/example/vynilos/views/AlbumsActivity.kt
+++ app/src/main/java/com/example/vynilos/views/AlbumsActivity.kt
@@ -2,120 +2,61 @@ package com.example.vynilos.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.vynilos.R
 import com.example.vynilos.adapters.AlbumAdapter
+import com.example.vynilos.apis.ApiService
+import com.example.vynilos.databinding.ActivityAlbumsBinding
 import com.example.vynilos.models.Album
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AlbumsActivity : AppCompatActivity() {
-    val albums = listOf(
-        Album(
-            id = 100,
-            name = "Buscando América",
-            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elektra"
-        ),
-        Album(
-            id = 101,
-            name = "Poeta del pueblo",
-            cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Recopilación de 27 composiciones del cosmos Blades que los bailadores y melómanos han hecho suyas en estos 40 años de presencia de los ritmos y concordias afrocaribeños en múltiples escenarios internacionales. Grabaciones de Blades para la Fania con las orquestas de Pete Rodríguez, Ray Barreto, Fania All Stars y, sobre todo, los grandes éxitos con la Banda de Willie Colón musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elekstra"
-        ),
-        Album(
-            id = 102,
-            name = "Buscando América",
-            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elektra"
-        ),
-        Album(
-            id = 103,
-            name = "Poeta del pueblo",
-            cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Recopilación de 27 composiciones del cosmos Blades que los bailadores y melómanos han hecho suyas en estos 40 años de presencia de los ritmos y concordias afrocaribeños en múltiples escenarios internacionales. Grabaciones de Blades para la Fania con las orquestas de Pete Rodríguez, Ray Barreto, Fania All Stars y, sobre todo, los grandes éxitos con la Banda de Willie Colón musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elekstra"
-        ),
-        Album(
-            id = 104,
-            name = "Buscando América",
-            cover = "https://www.udiscovermusic.com/wp-content/uploads/2019/11/a-day-at-the-races.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elektra"
-        ),
-        Album(
-            id = 105,
-            name = "Poeta del pueblo",
-            cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Recopilación de 27 composiciones del cosmos Blades que los bailadores y melómanos han hecho suyas en estos 40 años de presencia de los ritmos y concordias afrocaribeños en múltiples escenarios internacionales. Grabaciones de Blades para la Fania con las orquestas de Pete Rodríguez, Ray Barreto, Fania All Stars y, sobre todo, los grandes éxitos con la Banda de Willie Colón musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elekstra"
-        ),
-        Album(
-            id = 106,
-            name = "Buscando América",
-            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elektra"
-        ),
-        Album(
-            id = 107,
-            name = "Poeta del pueblo",
-            cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Recopilación de 27 composiciones del cosmos Blades que los bailadores y melómanos han hecho suyas en estos 40 años de presencia de los ritmos y concordias afrocaribeños en múltiples escenarios internacionales. Grabaciones de Blades para la Fania con las orquestas de Pete Rodríguez, Ray Barreto, Fania All Stars y, sobre todo, los grandes éxitos con la Banda de Willie Colón musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elekstra"
-        ),
-        Album(
-            id = 108,
-            name = "Buscando América",
-            cover = "https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elektra"
-        ),
-        Album(
-            id = 109,
-            name = "Poeta del pueblo",
-            cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Recopilación de 27 composiciones del cosmos Blades que los bailadores y melómanos han hecho suyas en estos 40 años de presencia de los ritmos y concordias afrocaribeños en múltiples escenarios internacionales. Grabaciones de Blades para la Fania con las orquestas de Pete Rodríguez, Ray Barreto, Fania All Stars y, sobre todo, los grandes éxitos con la Banda de Willie Colón musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983.",
-            genre = "Salsa",
-            recordLabel = "Elekstra"
-        ),
-
-    )
-    lateinit var mRecyclerView : RecyclerView
-    val mAdapter : AlbumAdapter = AlbumAdapter()
+    private lateinit var binding: ActivityAlbumsBinding
+    private lateinit var adapter: AlbumAdapter
+    private val albums = mutableListOf<Album>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_albums)
-        setUpRecyclerView()
+        binding = ActivityAlbumsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRecyclerView()
+        getAlbums()
     }
 
-    fun setUpRecyclerView() {
-        mRecyclerView = findViewById(R.id.rvAlbums) as RecyclerView
-        mRecyclerView.setHasFixedSize(true)
-        mRecyclerView.layoutManager = LinearLayoutManager(this)
-        mAdapter.AlbumAdapter(albums, this)
-        mRecyclerView.adapter = mAdapter
+    private fun initRecyclerView() {
+        adapter = AlbumAdapter(albums)
+        binding.rvAlbums.layoutManager = LinearLayoutManager(this)
+        binding.rvAlbums.adapter = adapter
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl("https://grupo-11-android.herokuapp.com/").
+                addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    private fun getAlbums() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call: Response<List<Album>> = getRetrofit().create(ApiService::class.java).getAlbums("/albums")
+            val albumsResponse: List<Album>? = call.body()
+            runOnUiThread {
+                if(call.isSuccessful) {
+                    val showAlbums = albumsResponse ?: emptyList()
+                    albums.clear()
+                    albums.addAll(showAlbums)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    showError()
+                }
+            }
+        }
+    }
+
+    private fun showError() {
+        Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show()
     }
 }
