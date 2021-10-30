a557e93b21d1572d49b613234701c31901da1e2f
Wenceslao Paez <wcpaez@gmail.com>
2021-10-29 20:54:48 -0500

    Add AlbumsActivity

---
+++ app/src/main/java/com/example/vynilos/views/AlbumsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vynilos.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vynilos.R
+
+class AlbumsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_albums)
+    }
+}
