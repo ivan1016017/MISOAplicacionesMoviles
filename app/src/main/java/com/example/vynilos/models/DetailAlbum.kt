fe3782708ca3f4a9cde520e6bf4f04864cc209fd
Mateo <m.castano1>
2021-11-02 17:58:44 -0500

    feature/NCPRJCT-103 Merged develop

---
+++ app/src/main/java/com/example/vynilos/models/DetailAlbum.kt
@@ -0,0 +1,9 @@
+package com.example.vynilos.models
+
+data class DetailAlbum(
+    val id: Number,
+    val name: String,
+    val image: String,
+    val description: String,
+    val creationDate: String,
+)
