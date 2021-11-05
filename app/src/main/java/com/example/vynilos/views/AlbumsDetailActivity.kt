c9b10f886a00644b51bebb58ce2cc4de7d3ca3ca
Mateo <m.castano1>
2021-11-05 00:34:15 -0500

    changes in the view and in some draw

--- app/src/main/java/com/example/vynilos/views/AlbumsDetailActivity.kt
+++ app/src/main/java/com/example/vynilos/views/AlbumsDetailActivity.kt
@@ -1,13 +1,14 @@
 package com.example.vynilos.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.vynilos.databinding.ActivityDetailAlbumBinding
 import com.example.vynilos.viewmodels.AlbumDetailViewModel
-import com.example.vynilos.viewmodels.AlbumsActivityViewModel
 import com.squareup.picasso.Picasso
 
 class AlbumsDetailActivity: AppCompatActivity() {
@@ -23,6 +24,13 @@ class AlbumsDetailActivity: AppCompatActivity() {
         if (albumId != null) {
             initViewModel(albumId.toInt())
         }
+        handleBackClick()
+    }
+
+    private fun handleBackClick() {
+        binding.toolbar.leftIcon.setOnClickListener { view ->
+            goToMainView(view)
+        }
     }
 
     private fun initViewModel(albumId: Number ) {
@@ -30,9 +38,17 @@ class AlbumsDetailActivity: AppCompatActivity() {
         viewModel.getLiveDataObserver().observe(this, Observer {
             binding.title.text = it.name
             binding.tvDescription.text = it.description
+            binding.gender.text = it.genre
+            binding.date.text = it.releaseDate
             Picasso.get().load(it.cover).into(binding.ivCover)
         })
 
         viewModel.makeApiCall(albumId)
+    }
+
+    private fun goToMainView(view: View) {
+        val intent = Intent(this, AlbumsActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
