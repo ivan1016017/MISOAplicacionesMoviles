6724380a965de8c9d6c84505ce3750f7ee979ec4
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-08 19:36:09 -0600

    collector adapter created

---
+++ app/src/main/java/com/example/vynilos/views/CollectorsActivity.kt
@@ -0,0 +1,68 @@
+package com.example.vynilos.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.vynilos.MainActivity
+import com.example.vynilos.R
+import com.example.vynilos.views.adapters.CollectorAdapter
+import com.example.vynilos.databinding.ActivityAlbumsBinding
+import com.example.vynilos.databinding.ActivityCollectorsBinding
+import com.example.vynilos.viewmodels.AlbumsActivityViewModel
+
+
+class CollectorsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCollectorsBinding
+    private lateinit var adapter: CollectorAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAlbumsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setToolbarText()
+        handleBackClick()
+        initViewModel()
+        initRecyclerView()
+    }
+
+    private fun setToolbarText() {
+        binding.toolbar.toolbarText.text = getString(R.string.albums)
+    }
+
+    private fun handleBackClick() {
+        binding.toolbar.leftIcon.setOnClickListener { view ->
+            goToMainView(view)
+        }
+    }
+
+    private fun initRecyclerView() {
+        adapter = AlbumAdapter()
+        binding.rvAlbums.layoutManager = LinearLayoutManager(this)
+        binding.rvAlbums.adapter = adapter
+    }
+
+    private fun initViewModel() {
+        val viewModel = ViewModelProvider(this).get(AlbumsActivityViewModel::class.java)
+        viewModel.getLiveDataObserver().observe(this, Observer {
+            adapter.setAlbums(it)
+            adapter.notifyDataSetChanged()
+        })
+        viewModel.makeApiCall()
+    }
+
+    private fun showError() {
+        Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun goToMainView(view: View) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/vynilos/views/adapters/CollectorAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.vynilos.views.adapters
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vynilos.R
+import com.example.vynilos.databinding.ItemArtistBinding
+import com.example.vynilos.models.Collector
+import com.squareup.picasso.Picasso
+
+class CollectorAdapter {
+}
