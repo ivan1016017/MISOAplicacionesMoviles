1b269ce461d1bb7d710be0f51dc58d2d5c20d4ce
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-08 20:05:41 -0600

    completing collector adapter

--- app/src/main/java/com/example/vynilos/models/Collector.kt
+++ app/src/main/java/com/example/vynilos/models/Collector.kt
@@ -3,6 +3,7 @@ package com.example.vynilos.models
 data class Collector (
     val id: Number,
     val name: String,
-    val telephone: Number,
-    val email: String
+    val telephone: String,
+    val email: String,
+    val image: String
         )
--- app/src/main/java/com/example/vynilos/views/adapters/CollectorAdapter.kt
+++ app/src/main/java/com/example/vynilos/views/adapters/CollectorAdapter.kt
@@ -10,9 +10,43 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vynilos.R
-import com.example.vynilos.databinding.ItemArtistBinding
+import com.example.vynilos.databinding.ItemCollectorBinding
 import com.example.vynilos.models.Collector
+
 import com.squareup.picasso.Picasso
 
-class CollectorAdapter {
+class CollectorAdapter (): RecyclerView.Adapter<CollectorAdapter.CollectorHolder>(){
+    private var collectors : List<Collector>? = null
+
+    override fun onBindViewHolder(holder: CollectorHolder, position: Int) {
+        val item = collectors?.get(position)
+        holder.bind(item!!)
+    }
+
+    fun setAlbums(collectorsList: List<Collector>) {
+        this.collectors = collectorsList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectorHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return CollectorHolder(layoutInflater.inflate(R.layout.item_collector, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        if(collectors == null) return 0
+        else return collectors?.size!!
+    }
+
+    class CollectorHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemCollectorBinding.bind(view)
+
+        fun bind(collector: Collector){
+            binding.tvCollectorName.text = collector.name
+            binding.tvTelephone.text = collector.telephone
+            Picasso.get().load(collector.image).into(binding.ivCollectorCover)
+            //itemView.setOnClickListener(View.OnClickListener { Toast.makeText(context, "Clicked on $artist.name", Toast.LENGTH_SHORT).show() })
+        }
+    }
+
+
 }
