2c39806c7d8363c726fcc524097c28aa577120e6
Wenceslao Paez <wcpaez@gmail.com>
2021-11-11 21:11:45 -0500

    Move repositories api calls to network_service_adapater

--- app/src/main/java/com/example/vynilos/repositories/ArtistsRepository.kt
+++ app/src/main/java/com/example/vynilos/repositories/ArtistsRepository.kt
@@ -9,19 +9,10 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class ArtistsRepository {
-    private var service = NetworkServiceAdapter.getRetrofitInstance().create(ApiService::class.java)
+    private var serviceAdapter = NetworkServiceAdapter()
 
     fun getArtists(liveDataList: MutableLiveData<List<Artist>>) {
-        val call = service.getArtists("/bands")
-
-        call.enqueue(object : Callback<List<Artist>> {
-            override fun onFailure(call: Call<List<Artist>>, t: Throwable) {
-                liveDataList.postValue(emptyList())
-            }
-            override fun onResponse(call: Call<List<Artist>>, response: Response<List<Artist>>) {
-                liveDataList.postValue(response.body())
-            }
-        })
+        serviceAdapter.getArtists(liveDataList)
     }
     fun getArtist(id: Number, liveDataList: MutableLiveData<Artist>) {
         val call = service.getArtist("/bands/$id")
