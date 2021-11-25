e81cbfd30e75778a78361566120fc6cda5474834
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-25 16:10:21 -0600

    setting the toolbar text of the view AlbumsTracksActivity

--- app/src/main/java/com/example/vynilos/views/AlbumsTracksActivity.kt
+++ app/src/main/java/com/example/vynilos/views/AlbumsTracksActivity.kt
@@ -3,10 +3,21 @@ package com.example.vynilos.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vynilos.R
+import com.example.vynilos.databinding.ActivityAlbumsTracksBinding
 
 class AlbumsTracksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlbumsTracksBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_albums_tracks)
+        binding = ActivityAlbumsTracksBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setToolbarText()
     }
+
+    private fun setToolbarText() {
+        binding.toolbar.toolbarText.text = getString(R.string.tie_track_to_album)
+    }
+
 }
