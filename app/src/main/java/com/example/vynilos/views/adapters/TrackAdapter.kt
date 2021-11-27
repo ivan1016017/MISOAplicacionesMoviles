f9f1ab4b0346bd51033bf84e52b37b30fd61d35c
Ivan Penaloza <ivan1016017@gmail.com>
2021-11-27 14:55:57 -0600

    Revert "errors commited"
    This reverts commit 89b9a54413db2ab9aa18751140bf169c37884612.

--- app/src/main/java/com/example/vynilos/views/adapters/TrackAdapter.kt
+++ app/src/main/java/com/example/vynilos/views/adapters/TrackAdapter.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vynilos.R
 import com.example.vynilos.databinding.ItemTrackBinding
+import com.example.vynilos.models.Album
 import com.example.vynilos.models.Track
-
+import com.example.vynilos.views.AlbumsDetailActivity
+import com.squareup.picasso.Picasso
 
 class TrackAdapter : RecyclerView.Adapter<TrackAdapter.TrackHolder>() {
     private var tracks : List<Track>? = null
@@ -39,6 +41,12 @@ class TrackAdapter : RecyclerView.Adapter<TrackAdapter.TrackHolder>() {
             binding.tvTrackName.text = track.name
             binding.tvTrackDuration.text = track.duration
 
+
+//            itemView.setOnClickListener {
+//                val intent = Intent(itemView.context, AlbumsDetailActivity::class.java)
+//                intent.putExtra("albumId", album.id.toString())
+//                itemView.context.startActivity(intent)
+//            }
         }
     }
 }
